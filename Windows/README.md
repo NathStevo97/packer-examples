664c465b0ddde6142c34239fee79bc242eacca42
NathStevo97 <natstephenson15@gmail.com>
2022-11-06 20:53:04 +0000

    Windows Server 2022 working for Gen 1 Hyper-V

--- Windows/README.md
+++ Windows/README.md
@@ -2,8 +2,8 @@
 |-------------------------|--------------------|---------------|---------------|------------|
 | Windows 11              | Pending            | Pending       | Pending       | Pending    |
 | Windows 10              | Working            | Working       | Working       | Working    |
-| Windows 2022 Standard   | Working            | Pending       | Pending       | Working    |
-| Windows 2022 Datacenter | Working            | Pending       | Pending       | Working    |
+| Windows 2022 Standard   | Working            | Working       | Pending       | Working    |
+| Windows 2022 Datacenter | Working            | Working       | Pending       | Working    |
 | Windows 2019 Standard   | Working            | Working       | Working       | Working    |
 | Windows 2019 Datacenter | Working            | Working       | Working       | Working    |
 | Windows 2016 Standard   | Working            | Working       | Working       | Working    |
---
+++ Windows/hv_win2022_dc.ps1
@@ -0,0 +1,44 @@
+# Build images
+0
+# Get Start Time
+$startDTM = (Get-Date)
+
+# Variables
+$template_file="./templates/win2022_dc.pkr.hcl"
+#$var_file="./variables/variables_win2022_dc.pkrvars.hcl"
+$machine="Windows Server 2022 datacenter Gen-1"
+$packer_log=0
+
+packer init "./required_plugins.pkr.hcl"
+#Write start time so you know how long it's been
+Write-Host "Start Time: = $startDTM" -ForegroundColor Yellow
+if ((Test-Path -Path "$template_file")) {
+  Write-Output "Template file found"
+  Write-Output "Building: $machine"
+  try {
+    $env:PACKER_LOG=$packer_log
+    #packer validate -var-file="$var_file" "$template_file"
+    packer validate -only='hyperv-iso.hv1-win2022-datacenter' "$template_file"
+  }
+  catch {
+    Write-Output "Packer validation failed, exiting."
+    exit (-1)
+  }
+  try {
+    $env:PACKER_LOG=$packer_log
+    packer version
+    #packer build --force -var-file="$var_file" "$template_file"
+    packer build -only='hyperv-iso.hv1-win2022-datacenter' --force "$template_file"
+  }
+  catch {
+    Write-Output "Packer build failed, exiting."
+    exit (-1)
+  }
+}
+else {
+  Write-Output "Template or Var file not found - exiting"
+  exit (-1)
+}
+
+$endDTM = (Get-Date)
+Write-Host "[INFO]  - Elapsed Time: $(($endDTM-$startDTM).totalseconds) seconds" -ForegroundColor Yellow
