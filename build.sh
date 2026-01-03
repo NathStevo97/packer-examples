37472962ea136b6c1587fc6a9c3b90a68c07b4a8
NathStevo97 <natstephenson15@gmail.com>
2026-01-03 20:09:03 +0000

    feat: start bash version of build.sh

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+# Variables
+#
+
+#
+# Debug
+#
+
+printf "%b\n" "Action: ${action}"
+
+printf "%b\n" "Log: ${log}"
+
+printf "%b\n" "Template: ${template}"
+
+printf "%b\n" "Version: ${version}"
+
+printf "%b\n" "Type: ${type}"
+
+printf "%b\n" "Provider: ${provider}"
+
+#
+# Variable Validation
+#
+
+#
+# Custom Variable Construction
+#
+
+# Initialize Logging
+
+# Init Plugins
+
+# Packer Validation
+
+# Packer Build
+
