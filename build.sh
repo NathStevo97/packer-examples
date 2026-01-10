20f39f04f5b3427da7ce7b321e77f12fb679074b
NathStevo97 <natstephenson15@gmail.com>
2026-01-10 16:35:47 +0000

    feat: build shell version

--- build.sh
+++ build.sh
@@ -1,38 +1,116 @@
 #!/bin/bash
 
 #
-# Variables
+# Variables (With Defaults)
 #
 
-#
-# Debug
-#
+# Input Args
 
-printf "%b\n" "Action: ${action}"
+action="${action:-build}"
+firmware="${firmware:-uefi}"
+log="${log:-0}"
+provider="${provider:-vmware-iso}"
+template="${template:-almalinux}"
+type="${type}"
+version="${version:-9}"
 
-printf "%b\n" "Log: ${log}"
+## General Vars
 
-printf "%b\n" "Template: ${template}"
+allowed_providers="vmware-iso virtualbox-iso hyperv-iso qemu"
 
-printf "%b\n" "Version: ${version}"
+## Logging
+logs_path="./logs"
 
-printf "%b\n" "Type: ${type}"
+export PACKER_LOG=$log
 
-printf "%b\n" "Provider: ${provider}"
+if [[ $log == 1 ]]; then
+    export PACKER_LOG=1
+fi
 
 #
 # Variable Validation
 #
 
+case "$provider" in
+    vmware-iso|virtualbox-iso|hyperv-iso|qemu)
+        printf "%b\n" "Valid provider: ${provider}"
+        ;;
+    *)
+        printf "Invalid provider: ${provider}"
+        exit 1
+        ;;
+esac
+
 #
 # Custom Variable Construction
 #
 
+var_file="./variables/${template}/${template}-${version}.pkrvars.hcl"
+template_file="./templates/${template}/${template}.pkr.hcl"
+
+## If template is windows: adjust var file for firmware
+# If Template is Windows-Server - Adjust Var File for Type
+if [[ ${template} == "windows" ]]; then
+    var_file="./variables/${template}/${template}-${version}-${firmware}.pkrvars.hcl"
+    if [[ $type != "" ]]; then
+        var_file="./variables/${template}/${template}-${version}-${type}-${firmware}.pkrvars.hcl"
+    fi
+fi
+
+# If Template is Windows-Server - Adjust Var File for Type
+if [[ ${template} == "windows-server" ]]; then
+    var_file="./variables/${template}/${template}-${version}-${firmware}.pkrvars.hcl"
+    if [[ $type != "" ]]; then
+        var_file="./variables/${template}/${template}-${version}-${type}-${firmware}.pkrvars.hcl"
+    fi
+fi
+
+#
+# Debug
+#
+
+packer --version
+
+printf "%b\n" "Action: ${action}"
+
+printf "%b\n" "Firmware: ${firmware}"
+
+printf "%b\n" "PACKER_LOG: ${PACKER_LOG}"
+
+printf "%b\n" "Provider: ${provider}"
+
+printf "%b\n" "Template: ${template}"
+
+printf "%b\n" "Template File: ${template_file}"
+
+printf "%b\n" "Type: ${type}"
+
+printf "%b\n" "Variable File: ${var_file}"
+
+printf "%b\n" "Version: ${version}"
+
 # Initialize Logging
+mkdir -p "${logs_path}"
+
+packer_log_path="${logs_path}/packerlog-${template}-${version}.txt"
+
+export PACKER_LOG_PATH="${packer_log_path}"
 
 # Init Plugins
+packer init "required_plugins.pkr.hcl"
+
+## Get Start Time
+start_time=$(date -u +%s.%N)
 
 # Packer Validation
+packer validate -var-file="${var_file}" -only="${provider}.${template}" "${template_file}"
 
 # Packer Build
 
+packer build -var-file="${var_file}" -only="${provider}.${template}" --force "${template_file}"
+
+end_time=$(date -u +%s.%N)
+
+elapsed_time=$(echo "$end_time - start_time" | bc -l)
+
+printf "%b\n" "Elapsed Time: ${elapsed_time} seconds"
